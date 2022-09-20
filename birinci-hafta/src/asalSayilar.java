728107fbbb30fea099f2d62cf6db28fd47642260
İmran Kaçan <70176770+imrankacan@users.noreply.github.com>
2022-09-20 23:42:40 +0300

    main

--- birinci-hafta/src/asalSayilar.java
+++ birinci-hafta/src/asalSayilar.java
@@ -1,22 +1,22 @@
 public class asalSayilar {
     public static void main(String[] args) {
         // 3 ile 100 arasında asal sayıların toplamını bulan program.
-        int toplam = 0;
-        for(int sayi=3;sayi<=100;sayi++)
+        int total = 0;
+        for(int number=3;number<=100;number++)
         {
-            int kontrol = 0;
-            for (int i = 2; i < sayi; i++) {
-                if (sayi % i == 0) {
-                    kontrol = 1;
+            int control = 0;
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    control = 1;
                     break;
                 }
             }
-            if(kontrol==0) {
-                toplam += sayi;
-                //System.out.printf("%d ,", sayi);
+            if(control==0) {
+                total += number;
+                //System.out.printf("%d ,", number);
             }
         }
-        System.out.printf("%nAsal sayıların toplamı: %d", toplam);
+        System.out.printf("%nAsal sayıların toplamı: %d", total);
     }
 }
 
