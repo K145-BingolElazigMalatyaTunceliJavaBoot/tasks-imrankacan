b971283ef9fb0ea96f0ae7d38ee0875e7d4a35c9
İmran Kaçan <70176770+imrankacan@users.noreply.github.com>
2022-09-20 23:45:19 +0300

    main

--- birinci-hafta/src/rastgeleSecim.java
+++ birinci-hafta/src/rastgeleSecim.java
@@ -6,16 +6,16 @@ public class rastgeleSecim {
              * Bu sayılardan 50 ile 100 arasında bir sayı bulunana kadar rastgele sayılar seçilir.
              * 50 ile 100 arasındaki sayının kaçıncı seçimde bulunduğunu bulan program.
              */
-            int a = 0 , sayac = 0;
+            int a = 0 , counter = 0;
             Random random = new Random();
             while (a<50 || a>100){
                 a = random.nextInt(500);
-                sayac++;
+                counter++;
                 /*a = (int)(Math.random()*500);  //math kütüphanesi ile
                 System.out.println(a);
                 System.out.println("sayac"+sayac);*/
            }
-                System.out.printf("%d sayısı %d. sırada bulunmuş.", a, sayac);
+                System.out.printf("%d sayısı %d. sırada bulunmuş.", a, counter);
         }
     }
 
