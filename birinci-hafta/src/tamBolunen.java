f5425090d5fc339667eb7748bf068050260bd27b
İmran Kaçan <70176770+imrankacan@users.noreply.github.com>
2022-09-20 23:46:11 +0300

    main

--- birinci-hafta/src/tamBolunen.java
+++ birinci-hafta/src/tamBolunen.java
@@ -1,15 +1,15 @@
 public class tamBolunen {
     public static void main (String []args){
         //200 ile 500 arasındaki 3 ile tam bölünen sayılar dışındaki sayıların toplamını bulan program.
-        int toplam = 0;
+        int total = 0;
         for (int i = 200; i <=500 ; i++) {
             if(i%3==0){
                 continue;
             }else {
                 //System.out.printf("%d %n",i);
-                toplam+=i;
+                total+=i;
             }
         }
-        System.out.printf("200 ile 500 arasındaki 3 ile tam bölünen sayılar dışındaki sayıların toplamını: %d", toplam);
+        System.out.printf("200 ile 500 arasındaki 3 ile tam bölünen sayılar dışındaki sayıların toplamını: %d", total);
     }
 }
